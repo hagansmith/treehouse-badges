2f3f576bd5827e7b975b967fb4bd502cfb853d91
Adam Smith <hagansmith@gmail.com>
2017-10-07 13:29:29 -0500

    added terrible styling

--- javascripts/dom.js
+++ javascripts/dom.js
@@ -5,14 +5,14 @@ const domBuilder =  (data) => {
   let fullString = "";
   for (var i = 0; i < data.length; i++) {
   let string = "";
-  string += `<img class="col-md-1" src="${data[i].icon_url}">`;
+  string += `<img class="col-md-1" id="badge" src="${data[i].icon_url}" >`;
   fullString += string;
   }
   printToDom(fullString);
 };
 
 const printToDom = (string) => {
-  $("#badgeHolder").html(string);
+  $(".badgeHolder").html(string);
 };
 
 
